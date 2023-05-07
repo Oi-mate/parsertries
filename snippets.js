d3af84378c930af97e5dfc80afb183e98fbbbbe9
Mikhail Kolbeshin <mkolbeshin@codeitup.ru>
2023-05-07 13:00:45 +0300

    tries

---
+++ snippets.js
@@ -0,0 +1,6 @@
+// https://dromex.ru/parts/4U/MRP61611
+Array.from(document.querySelectorAll('img')).filter(x => x.getAttribute('alt') && x.getAttribute('alt').includes('MRP'))
+
+// https://autopiter.ru/goods/tysb00253
+document.querySelector('input.SearchForm__searchBarInput___1jnSK');
+window.location.href = 'https://autopiter.ru/goods/mrp61611'
